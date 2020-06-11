f9dbe4cd932234685cd349422143b394e084dffa
Gary30315 <66403395+Gary30315@users.noreply.github.com>
2020-06-11 22:39:15 +0800

    Create Day_003_HW.py

---
+++ Day_003_HW.py
@@ -0,0 +1,11 @@
+"""
+作業1:目標
+希望提升不管是高資源語言或低資源語言的Blue評分(提升翻譯質量)
+
+作業2:使用方法
+2016年使用GNMT模型，之後使用RNN架構下的GNMT模型。再接著是使用lingvo進行編碼解碼。另外也使用M4架構對低資源語言進行翻譯
+
+作業3:資料來源
+文章URL"https://ai.googleblog.com/2020/06/recent-advances-in-google-translate.html"
+
+"""
