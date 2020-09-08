fdfd8dd108c8248f44157a1a1a58758c9f738011
Gary30315 <66403395+Gary30315@users.noreply.github.com>
2020-09-08 15:47:42 +0800

    Update Day_0013_HW.py

--- Day_0013_HW.py
+++ Day_0013_HW.py
@@ -13,14 +13,18 @@ print(app_train['CNT_CHILDREN_GROUP'].value_counts())
 print(app_train['CNT_CHILDREN_GROUP'].head())
 
 #作業2-1
-grp = app_train['CNT_CHILDREN_GROUP']
+grp = ['CNT_CHILDREN_GROUP','TARGET']
 grouped_df = app_train.groupby(grp)['AMT_INCOME_TOTAL']
 print(grouped_df.mean())
 
 #作業2-2
 
-plt_column = app_train.groupby(grp)['AMT_INCOME_TOTAL']
-plt_by = grouped_df.mean()
+plt_column = 'AMT_INCOME_TOTAL'
+plt_by = ['CNT_CHILDREN_GROUP', 'TARGET']
 app_train.boxplot(column=plt_column,by = plt_by, showfliers = False, figsize=(12,12))
 plt.suptitle('boxplot')
 plt.show()
+
+#作業3
+app_train['AMT_INCOME_TOTAL_Z_BY_CHILDREN_GRP-TARGET'] = grouped_df.apply(lambda x:(x-np.mean(x))/np.std(x))
+print(app_train['AMT_INCOME_TOTAL_Z_BY_CHILDREN_GRP-TARGET'].head())
