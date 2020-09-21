c64eaa25a08bc3db7c7e59c5afda5896bc192630
Gary30315 <66403395+Gary30315@users.noreply.github.com>
2020-09-21 19:48:24 +0800

    Create Day_0019_HW.py

---
+++ Day_0019_HW.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import warnings 
+warnings.filterwarnings('ignore')
+
+app_train = pd.read_csv('application_train.csv')
+
+unique_house_type = app_train['HOUSETYPE_MODE']
+
+nrows = len(unique_house_type)
+ncols = nrows // 2
+
+plt.figure(figsize=(10,30))
+for i in range(len(unique_house_type)):
+    plt.subplot(nrows, ncols, i+1)
+
+    app_train.loc[i,'AMT_CREDIT '].hist()
+    
+    plt.title(str(unique_house_type[i]))
+plt.show()   
