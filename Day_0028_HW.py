9d32dd9c830f940d373cdfac3b5bd2cc2d7c52a0
Gary30315 <66403395+Gary30315@users.noreply.github.com>
2020-12-16 21:20:44 +0800

    Update Day_0028_HW.py

--- Day_0028_HW.py
+++ Day_0028_HW.py
@@ -25,7 +25,10 @@ df['longitude_diff'] = df['dropoff_longitude'] - df['pickup_longitude']
 df['latitude_diff'] = df['dropoff_latitude'] - df['pickup_latitude']
 df['distance_2D'] = (df['longitude_diff']**2 + df['latitude_diff']**2)**0.5
 
-df['ratio'] = df['pickup_longitude']*0.75756
+#作業1
+latitude_average = df['pickup_latitude'].mean()
+latitude_factor = math.cos(latitude_average/180*math.pi)
+df['distance_real'] = ((df['longitude_diff']*latitude_factor)**2 + df['latitude_diff']**2)**0.5
 
 df = df.drop(['pickup_datetime'] , axis=1)
 scaler = MinMaxScaler()
@@ -35,5 +38,6 @@ print(f'Linear Reg Score : {cross_val_score(Linear, train_X, train_Y, cv=5).mean
 GDBT = GradientBoostingRegressor()
 print(f'Gradient Boosting Reg Score : {cross_val_score(GDBT, train_X, train_Y, cv=5).mean()}')
 
-
+#作業2
+#準確率下降
 
